bbbb65c67e639213eaf9ef17991b1f3523c519be
shubham <shubhamrewamp17@gmail.com>
2025-08-30 02:16:15 +0530

    number data type learned

---
+++ Datatypes/NumberdataTypes/index.ts
@@ -0,0 +1,20 @@
+// Number Data Type
+// • Apply number data type
+// • Redeclare issue
+// • How to Add number with data type  
+// • How to use binary and hexadecimal octa numbers  = 0b1010 , 0x1A , 0o12
+// • Convert string to number
+// • Type Inference with number
+// • Decimal with number data type
+
+var num1 : number = 10 ;
+var num2 : number = 20 ;
+
+
+var newNum : number = num1 + num2  ;
+
+console.log("this sum of two number is => " + newNum)   
+
+
+
+
