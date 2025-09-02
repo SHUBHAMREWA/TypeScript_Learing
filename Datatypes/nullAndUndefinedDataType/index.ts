1f8f64802341e4309f55729c69c659b2545d5206
shubham <shubhamrewamp17@gmail.com>
2025-09-02 14:41:07 +0530

    null and Undefinded data types and Config setup

---
+++ Datatypes/nullAndUndefinedDataType/index.ts
@@ -0,0 +1,31 @@
+
+var data : null | string  = null ;
+
+//console.log( "null ka type of hamesaha Object hota hai " , data)
+
+data = "shubham"
+console.log(typeof data)
+
+
+
+// using undefined data type 
+var myName:undefined | string = undefined ; 
+
+console.log("undefined ka type of hamesaha undefined hota hai")
+
+myName = "shubham"  
+
+console.log(typeof myName)
+
+
+
+// compaire Null and Undefined Data type together 
+
+var thisIsNull : null = null  ;
+var thisisUndefined : undefined = undefined ;  
+
+console.log("===>>>double equal ka use karne pr true ayenga compaire karne pr ⚠️⚠️ console.log(thisIsNull == thisisUndefined) " , (thisIsNull == thisisUndefined) , "because ", "==" , "ko jab use karte hai toh ye inko binary me convert karta hai aur ye 0 == 0 hote hai aur ye true hai ") ;
+
+
+console.log("===>>>triple equal ka use karne pr false ayenga compaire karne pr ⚠️⚠️ console.log(thisIsNull === thisisUndefined) " , (thisIsNull === thisisUndefined) , "===" , "ko jab use karte hai tab ye memore location ko check karta hai aur dono variable ka memory location different hota hai esliye ye false ho jata hai")
+
