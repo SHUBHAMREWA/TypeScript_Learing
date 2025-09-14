929f32b607091be787d1e3ae0bc66b012d9627d6
shubham <shubhamrewamp17@gmail.com>
2025-09-14 21:05:26 +0530

    Any and Unkown data type Learned

---
+++ Datatypes/Any_unknown_datatypes/index.ts
@@ -0,0 +1,22 @@
+
+
+var data1 : any = 10 ; 
+
+ data1 = true  ;
+ data1 = ["shubham" , 10 , true]    
+ data1   = "shubham" ;  
+
+
+
+
+ console.log(data1.toString().toUpperCase())  ;
+
+
+ var data2 : unknown    = 10 ; 
+   
+      if( typeof data2 == "number"){
+            console.log(typeof data2.toString()) ;
+      }
+    //  console.log(data2.toString()) ;
+
+
---
+++ Datatypes/Any_unknown_datatypes/outputJs/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var data1 = 10;
+data1 = true;
+data1 = ["shubham", 10, true];
+data1 = "shubham";
+console.log(data1.toString().toUpperCase());
+var data2 = 10;
+if (typeof data2 == "number") {
+    console.log(typeof data2.toString());
+}
+//  console.log(data2.toString()) ;
