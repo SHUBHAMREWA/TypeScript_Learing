929d30828403add5f27740067a5e6a4b1445bffb
shubham <shubhamrewamp17@gmail.com>
2025-09-20 18:39:08 +0530

    pipe operator learned

---
+++ Datatypes/union_data_type/index.ts
@@ -0,0 +1,49 @@
+
+
+//  Union data type   ==> 
+//  we use Union data type for Store MORE then one value in Any variable 
+//  this is represented By symbol of pie ( | )
+ 
+
+
+var dataUnion: number | string = "kapil"  ;
+
+dataUnion = 12
+
+console.log(dataUnion) ;
+
+
+// spread / rest operation testing    
+ 
+// rule 1. we are using spred  ... operation for in Function parametes for combile all argument in Array 
+// Example -->  
+   
+ function  addNumbers (...items : number[]){
+     
+      return  items.reduce((acc, cur)=>{
+             return  acc + cur
+        } , 0)
+       
+
+      
+ }
+
+ let value: number = addNumbers(2,3,4,5) ;
+
+ console.log("this a value from ... operator use in function parameter", value) ; 
+
+
+//   rule 2 => we are using rest ... operation for in function argument when we need to sepret all array or object value 
+
+const addValues  = (val1 : number , val2 : number , val3 : string)=>{    
+     
+       return val1 + val2  + val3     
+      
+}
+
+
+let datas = [1,2]
+
+let values = addValues(...datas , "< = >ye work kr raha hai")
+
+console.log(values) ;   
---
+++ Datatypes/union_data_type/output/index.js
@@ -0,0 +1,25 @@
+"use strict";
+//  Union data type   ==> 
+//  we use Union data type for Store MORE then one value in Any variable 
+//  this is represented By symbol of pie ( | )
+Object.defineProperty(exports, "__esModule", { value: true });
+var dataUnion = "kapil";
+dataUnion = 12;
+console.log(dataUnion);
+// spread / rest operation testing    
+// rule 1. we are using spred  ... operation for in Function parametes for combile all argument in Array 
+// Example -->  
+function addNumbers(...items) {
+    return items.reduce((acc, cur) => {
+        return acc + cur;
+    }, 0);
+}
+let value = addNumbers(2, 3, 4, 5);
+console.log("this a value from ... operator use in function parameter", value);
+//   rule 2 => we are using rest ... operation for in function argument when we need to sepret all array or object value 
+const addValues = (val1, val2, val3) => {
+    return val1 + val2 + val3;
+};
+let datas = [1, 2];
+let values = addValues(...datas, "< = >ye work kr raha hai");
+console.log(values);
