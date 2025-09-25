8b02c798dad5a3c9afd7f41ddf54f9bb42bfaf00
shubham <shubhamrewamp17@gmail.com>
2025-09-25 16:55:53 +0530

    enum data type learned

---
+++ Datatypes/enum_datatype/index.ts
@@ -0,0 +1,10 @@
+
+
+enum myPersonalData{
+      student = "student" , 
+      management = "management" 
+}
+
+var info: myPersonalData = myPersonalData.management ;
+
+console.log(info)
---
+++ Datatypes/enum_datatype/output/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var myPersonalData;
+(function (myPersonalData) {
+    myPersonalData["student"] = "student";
+    myPersonalData["management"] = "management";
+})(myPersonalData || (myPersonalData = {}));
+var info = myPersonalData.management;
+console.log(info);
