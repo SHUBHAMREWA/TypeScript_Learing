6db7e0c1b22fd8293ca4cc7e157d7f4fa8ab317d
shubham <shubhamrewamp17@gmail.com>
2025-09-26 17:20:16 +0530

    dom element selector learned

---
+++ Domhandling/dom.ts
@@ -0,0 +1,8 @@
+
+
+var buttonTag  = document.querySelector("button") ;
+ console.log(buttonTag?.textContent)
+
+ var anchorTag = document.querySelector(".anchorTag")! as HTMLAnchorElement ;
+
+ console.log(anchorTag.href)
---
+++ Domhandling/output/dom.js
@@ -0,0 +1,5 @@
+var buttonTag = document.querySelector("button");
+console.log(buttonTag === null || buttonTag === void 0 ? void 0 : buttonTag.textContent);
+var anchorTag = document.querySelector(".anchorTag");
+console.log(anchorTag.href);
+export {};
